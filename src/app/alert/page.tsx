752ab1fe6eddd98a2afd34222ebc9fa0624df7f3
choidy180 <choidy180@naver.com>
2025-01-20 02:24:35 +0900

    'deploy'

---
+++ src/app/alert/page.tsx
@@ -0,0 +1,11 @@
+const AccodianConponentsPage = () => {
+    return (
+        <div className="w-full max-w-[1000px] flex flex-col justify-start items-center">
+            <title>Alert</title>
+            <h2 className="text-[3rem] mt-20">Alert</h2>
+            
+        </div>
+    )
+}
+
+export default AccodianConponentsPage;
