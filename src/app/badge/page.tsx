e1dd60f272c538b35afec9030bf0c7bbb9233e31
choidy180 <choidy180@naver.com>
2025-01-22 21:15:43 +0900

    'deploy'

--- src/app/badge/page.tsx
+++ src/app/badge/page.tsx
@@ -1,3 +1,5 @@
+import DefaultBadge from "../components/badge/default-badge";
+
 const BadgeComponentsPage = () => {
     return (
         <div className="w-full max-w-[1000px] flex flex-col justify-start items-center mb-[100px]">
@@ -5,6 +7,7 @@ const BadgeComponentsPage = () => {
             <h2 className="text-[3rem] mt-20">Badge</h2>
             <h4 className="text-[1.25rem] mt-5 mb-10">Badge는 사용자 인터페이스에서 주로 작은 시각적 표시로 사용되며, 추가 정보나 상태를 전달합니다. Badge는 상황에 따라 정보 전달, 상태 표시, 성과 표현 등의 용도로 사용됩니다.</h4>
 
+            <DefaultBadge/>
         </div>
     )
 }
